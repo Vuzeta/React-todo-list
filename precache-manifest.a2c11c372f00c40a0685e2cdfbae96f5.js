207e722e2ac77cb5b6380e53fd19958e88ea86f1
Vuzeta95 <machnio95@o2.pl>
2019-06-25 22:05:53 +0200

    Updates

---
+++ precache-manifest.a2c11c372f00c40a0685e2cdfbae96f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8526ab12f2abbba2a1e4e7f8410f87d",
+    "url": "/React-todo-list/index.html"
+  },
+  {
+    "revision": "4cdadcff42134100463e",
+    "url": "/React-todo-list/static/css/main.835dbd84.chunk.css"
+  },
+  {
+    "revision": "27a3a9b8729ad88e7be6",
+    "url": "/React-todo-list/static/js/2.3d066136.chunk.js"
+  },
+  {
+    "revision": "4cdadcff42134100463e",
+    "url": "/React-todo-list/static/js/main.22b02dae.chunk.js"
+  },
+  {
+    "revision": "a26ea031b757c28c4998",
+    "url": "/React-todo-list/static/js/runtime~main.884aa81e.js"
+  }
+]);
